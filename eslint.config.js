56140ff7ff173bab8db741d48ed503adbbba8802
cholulu99 <tttjdrudwkd0@naver.com>
2025-06-01 02:59:41 +0900

    a

--- eslint.config.js
+++ eslint.config.js
@@ -1,45 +1,63 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import prettierPlugin from 'eslint-plugin-prettier';
-import airbnbBase from 'eslint-config-airbnb-base';
-import airbnbReact from 'eslint-config-airbnb/rules/react';
+import eslintPluginReact from "eslint-plugin-react";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import eslintPluginStorybook from "eslint-plugin-storybook";
+import eslintPluginImport from "eslint-plugin-import";
 
 export default [
-  { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["*.js", "*.jsx"],
+    extends: [
+      "airbnb",
+      "plugin:react/recommended",
+      "plugin:prettier/recommended",
+      "plugin:storybook/recommended",
+    ],
+    plugins: {
+      react: eslintPluginReact,
+      prettier: eslintPluginPrettier,
+      storybook: eslintPluginStorybook,
+      import: eslintPluginImport,
+    },
     languageOptions: {
-      ecmaVersion: 'latest',
-      globals: globals.browser,
-      sourceType: 'module',
       parserOptions: {
-        ecmaFeatures: { jsx: true },
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
       },
     },
-    plugins: {
-      'react': react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      'prettier': prettierPlugin,
-    },
     rules: {
-      ...js.configs.recommended.rules,
-      ...airbnbBase.rules,
-      ...airbnbReact.rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/react-in-jsx-scope': 'off',
-      'prettier/prettier': ['error', { endOfLine: 'lf' }],
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'import/extensions': ['error', 'ignorePackages', { js: 'never', jsx: 'never' }],
-      'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.test.jsx', '**/*.test.js'] }],
+      "react/react-in-jsx-scope": "off",
+      "prettier/prettier": [
+        "error",
+        {
+          endOfLine: "lf",
+        },
+      ],
+      "import/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          js: "never",
+          jsx: "never",
+        },
+      ],
+      "import/no-extraneous-dependencies": [
+        "error",
+        {
+          devDependencies: ["**/*.test.jsx", "**/*.test.js"],
+        },
+      ],
     },
     settings: {
-      react: { version: 'detect' },
-      'import/resolver': {
-        node: { extensions: ['.js', '.jsx'] },
+      react: {
+        version: "detect",
+      },
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx"],
+        },
       },
     },
   },
